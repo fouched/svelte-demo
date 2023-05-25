41d71e3d383a997e97dcbf2391f6cc7b2e29e9fb
Fouché du Preez <fouche@unifi.credit>
2023-05-25 17:08:55 +0200

    init

---
+++ src/App.svelte
@@ -0,0 +1,7 @@
+<script>
+import Fun from './Fun.svelte';
+let fontSize = 150
+</script>
+<h1>Font Size: {fontSize}</h1>
+<Fun bind:fontSize />
+
---
+++ src/Fun.svelte
@@ -0,0 +1,11 @@
+<script>
+    export let fontSize = 100;
+    setInterval(() => {
+        if (fontSize === 150) {
+            fontSize = 5;
+        } else {
+            fontSize += 1;
+        }
+    })
+</script>
+<h2 style="font-size: {fontSize}px">You rock!</h2>
